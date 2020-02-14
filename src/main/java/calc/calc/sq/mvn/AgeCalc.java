51fafaf7beee35b7f7aff1bf495948bb4e4cc601
Training_b6b.01.01 <Training_b6b.01.01@BDC6-D-5DSLXH2.dir.svc.accenture.com>
2020-02-14 12:07:00 +0530

    1410

--- src/main/java/calc/calc/sq/mvn/AgeCalc.java
+++ src/main/java/calc/calc/sq/mvn/AgeCalc.java
@@ -1,7 +1,6 @@
 package calc.calc.sq.mvn;
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.Arrays;
 import java.io.Externalizable;
 import java.io.IOException;
 import java.io.ObjectInput;
@@ -9,18 +8,15 @@ import java.io.ObjectOutput;
 import java.time.DateTimeException;
 
 public class AgeCalc implements Externalizable {
-	private String inputDate;
-	public AgeCalc(String inputDate){
-		this.inputDate = inputDate;
-	}
+		public AgeCalc(String inputDate){
+		}
 	public static String calculateAge(String inputDate) throws Exception{
 		AgeCalc obj = new AgeCalc(inputDate);
 		String temp[] = inputDate.split("-");
 		String date1 = temp.toString();
 		System.out.println(date1);
 		String resultString = "You are ";
-		int sum = 0;
-	    LocalDate todayDate = LocalDate.now();
+			    LocalDate todayDate = LocalDate.now();
 	    try {
 	    	LocalDate dobDate = LocalDate.of(Integer.parseInt(temp[0]), Integer.parseInt(temp[1]), Integer.parseInt(temp[2]));
 		    Period intervalPeriod = Period.between(dobDate, todayDate);
@@ -31,13 +27,11 @@ public class AgeCalc implements Externalizable {
 		    	throw new DateTimeException("Invalid day exception");
 		    }
 		    if(months < 0){
-		    	sum=++sum;
-		    	System.out.println(obj.getClass());
+		    		    	System.out.println(obj.getClass());
 		    	throw new DateTimeException("Invalid month exception");
 		    }
 		    if(years < 0){
-		    	sum=+10;
-		    	throw new DateTimeException("Invalid year exception");
+		    			    	throw new DateTimeException("Invalid year exception");
 		    }
 		    if(years != 0) {
 		    	if(years == 1)
